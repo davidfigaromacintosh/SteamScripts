24f726b806a28e3e7d061de04fbd91a7e298881e
David Macintosh <dawid9554@gmail.com>
2020-08-31 20:06:38 +0200

    Update JoinAllProfileGroups.user.js

--- JoinAllProfileGroups.user.js
+++ JoinAllProfileGroups.user.js
@@ -8,14 +8,15 @@
 // @description 31.08.2020, 19:50:59
 // ==/UserScript==
 
-function DFM_JG(){
+(()=>{$J('.profile_group_links.profile_count_link_preview_ctn.responsive_groupfriends_element')
+.append('<a id="DFM_JG"class="btn_profile_action btn_medium"><span>Join all groups</span></a>');
+
+$J('#DFM_JG').click(function(){
   $J.get(location.href+'?xml=1').done(d=>{
     $J(d).find('groupID64').each((i,e)=>{
-        $J.post('https://steamcommunity.com/gid/'+$J(e).text(),
-        {action:'join',sessionID:g_sessionID})
-        .done(console.log('Joined the group!')).fail('Error while joining this group!');
+      $J.post('https://steamcommunity.com/gid/'+$J(e).text(),
+      {action:'join',sessionID:g_sessionID})
+      .done(console.log('Joined the group!')).fail('Error while joining this group!');
     });
-}).fail('Error retrieving group data!');}
-
-(()=>$J('.profile_group_links.profile_count_link_preview_ctn.responsive_groupfriends_element')
-.append('<a href="javascript:DFM_JG()"class="btn_profile_action btn_medium"><span>Join all groups</span></a>'))();
+  }).fail('Error retrieving group data!');});
+})();
