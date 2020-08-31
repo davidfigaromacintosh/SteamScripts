0b3b5a28aba29c88245425f1e12d060e14edf1fd
David Macintosh <dawid9554@gmail.com>
2020-08-31 20:00:22 +0200

    Create JoinAllProfileGroups.user.js

---
+++ JoinAllProfileGroups.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Join All Profile Groups
+// @grant       none
+// @include     https://steamcommunity.com/profiles/*
+// @include     https://steamcommunity.com/id/*
+// @version     1.0
+// @author      David Mac
+// @description 31.08.2020, 19:50:59
+// ==/UserScript==
+
+const DFM_JG=()=>{
+  $J.get(location.href+'?xml=1').done(d=>{
+    $J(d).find('groupID64').each((i,e)=>{
+        $J.post('https://steamcommunity.com/gid/'+$J(e).text(),
+        {action:'join',sessionID:g_sessionID})
+        .done(console.log('Joined the group!')).fail('Error while joining this group!');
+    });
+}).fail('Error retrieving group data!');}
+
+(()=>$J('.profile_group_links.profile_count_link_preview_ctn.responsive_groupfriends_element')
+.append('<a href="javascript:DFM_JG()"class="btn_profile_action btn_medium"><span>Join all groups</span></a>'))();
